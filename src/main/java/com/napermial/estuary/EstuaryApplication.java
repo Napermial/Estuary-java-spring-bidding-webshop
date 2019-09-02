7c0be853ae8e84897096d2d642df792fc709b244
Napermial <hatvani98@freemail.hu>
2019-09-02 18:34:31 +0200

    base application + test

---
+++ src/main/java/com/napermial/estuary/EstuaryApplication.java
@@ -0,0 +1,13 @@
+package com.napermial.estuary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstuaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstuaryApplication.class, args);
+    }
+
+}
