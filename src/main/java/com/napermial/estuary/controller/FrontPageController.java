c791f2bfdc354d303fa0cabebc950e5cb77ccfea
Napermial <h>
2019-09-04 19:25:18 +0200

    model/context added

--- src/main/java/com/napermial/estuary/controller/FrontPageController.java
+++ src/main/java/com/napermial/estuary/controller/FrontPageController.java
@@ -1,13 +1,30 @@
 package com.napermial.estuary.controller;
 
+import com.napermial.estuary.dao.POJOimplementation.AdvertStorageImplementation;
+import com.napermial.estuary.model.Advert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class FrontPageController {
 
-    @RequestMapping("/")
-    String frontPageController(){
+    @Autowired
+    AdvertStorageImplementation advertStorageImplementation;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    String frontPageController() {
         return "index";
     }
+
+    @ModelAttribute("adverts")
+    List<Advert> adverts(){
+        return advertStorageImplementation.listAdverts();
+    }
+
 }
