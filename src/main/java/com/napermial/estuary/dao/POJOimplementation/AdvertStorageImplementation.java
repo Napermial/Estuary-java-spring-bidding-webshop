832d007dcf01e384b5ce0a5b1f153e8db833948a
Napermial <h>
2019-09-04 19:25:38 +0200

    refactor

--- src/main/java/com/napermial/estuary/dao/POJOimplementation/AdvertStorageImplementation.java
+++ src/main/java/com/napermial/estuary/dao/POJOimplementation/AdvertStorageImplementation.java
@@ -13,10 +13,6 @@ public class AdvertStorageImplementation implements AdvertStorage {
 
     private List<Advert> adverts = new ArrayList<>();
 
-    private List<Advert> adverts = new ArrayList<>();
-
-    public AdvertStorageImplementation() {
-    }
 
     public AdvertStorageImplementation() {
     }
